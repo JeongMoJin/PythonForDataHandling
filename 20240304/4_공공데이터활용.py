85fb9d2cf20c544d52c88e25d507f5c2b192ca69
JeongMoJin <wlswjdah123@naver.com>
2024-03-04 17:25:12 +0900

    2024-03-04

---
+++ 20240304/4_공공데이터활용.py
@@ -0,0 +1,72 @@
+import csv
+from typing import List
+
+# example_file = open('./input/아파트(매매)_실거래가_20240304154554.csv')
+# example_reader = csv.DictReader(example_file)
+# print('출력')
+# for row in example_reader:
+#     print(f'{row}')
+# print('=' * 20)
+
+# 조건 : 래미안 단지만 검색
+# 출력조건: 지역, 단지명, 크기, 층수, 거래금액
+# 저장 파일명 : apt_2403_조건1.csv
+# with로 파일 처리
+
+new_datas: List[dict] = list()
+with open('./input/아파트(매매)_실거래가_20240304154554.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        row: dict
+        if row['단지명'].find('래미안') != -1:
+            print(f'{row["단지명"]}')
+            new_data: dict = dict()
+            new_data['시군구'] = row['시군구']
+            new_data['단지명'] = row['단지명']
+            new_data['전용면적(㎡)'] = row['전용면적(㎡)']
+            new_data['층'] = row['층']
+            new_data['거래금액(만원)'] = row['거래금액(만원)']
+            new_datas.append(new_data)
+print(new_datas)
+header: list = list(new_datas[0].keys())
+
+file_name = 'apt_2403_조건1.csv'
+with open(f'./output/{file_name}', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=header)
+    writer.writeheader()
+    for data in new_datas:
+        writer.writerow(data)
+print(f'{file_name}파일이 생성 되었습니다.')
+
+
+# 조건 : 롯데캐슬 거래건만 검색
+# 출력조건 : 구(구만 나오도록 할 것), 단지명, 전용면적, 층, 거래금액
+# 저장 파일명 : apt_2403_조건3.csv
+# with로 파일 처리
+# 예) 달서구, 롯데캐슬레이크, 84, 91, 3, "32.109"
+
+new_datas: List[dict] = list()
+with open('./input/아파트(매매)_실거래가_20240304154554.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        row: dict
+        if row['단지명'].find('롯데캐슬') >= 0:
+            print(f'{row["시군구"].split(" ")[1]} / {row["단지명"]}')
+            new_data: dict = dict()
+            new_data['구'] = row['시군구'].split(" ")[1]
+            new_data['시군구'] = row['시군구']
+            new_data['단지명'] = row['단지명']
+            new_data['전용면적(㎡)'] = row['전용면적(㎡)']
+            new_data['층'] = row['층']
+            new_data['거래금액(만원)'] = row['거래금액(만원)']
+            new_datas.append(new_data)
+print(new_datas)
+header: list = list(new_datas[0].keys())
+
+file_name = 'apt_2403_조건2.csv'
+with open(f'./output/{file_name}', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=header)
+    writer.writeheader()
+    for data in new_datas:
+        writer.writerow(data)
+print(f'{file_name}파일이 생성 되었습니다.')
